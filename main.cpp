30636be5b2fa4538c26a0a2a9141312ace36e1ba
Andrew Garcia <andrewgarcia@Andrews-MBP.ht.home>
2024-09-15 14:33:04 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    int hello = 0;
+
+    if(hello == 0){
+        cout <<"sup"<<endl;
+    }
+
+    else{
+        cout<<"nope"<<endl;
+    }
+    return 0;
+}
