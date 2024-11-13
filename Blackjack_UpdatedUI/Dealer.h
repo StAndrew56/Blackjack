4b73f6b3a06af25a663ef3d069cc963729544d84
Andrew Garcia <andrew02gar@gmail.com>
2024-11-12 20:53:53 -0600

    More BUTTONS
    Added Stand and Double down as well as an end game function and added it to each and every button when an end game condition is met.
    
    Also organized MainWindow.cpp code!

--- Blackjack_UpdatedUI/Dealer.h
+++ Blackjack_UpdatedUI/Dealer.h
@@ -8,26 +8,40 @@
 
 #include "Deck.h" //For interaction with main Deck
 #include "Cards.h" //To create a vector of cards as a Dealer's hand
-#include "User.h"
+#include "User.h" //To get User's hand value
 #include <iostream>
 #include <string>
 #include <vector> //Dealer uses vectors
 
 using namespace std;
 
-class Dealer {
+
+class Dealer : public QObject{
+    Q_OBJECT
 
 private:
     int dealerHandVal;
-    int UserHandVal;
+    int userHandVal;
     vector<Cards> dealerHand; //Dealer's hand vector of cards
-    Deck mainDeck;
     int dealerAceCount = 0;
+    Deck mainDeck;
+    const int down = 1;
+    const int up = 0;
 
 public:
+
+
+
     Dealer(); //Constructor
 
-    //Param: Player+Dealer count
+    //TEXT OUTPUT MEMBER FUNCTIONS-----------------
+    void printRankDealer(Rank cardRank);
+    void printSuitDealer(Suit suit);
+    void printDealerHand();
+    void trueRank();
+    //TEXT OUTPUT MEMBER FUNCTIONS-----------------
+
+    //Param: User's hand, deck
     void dealCards(vector<Cards>& playerHand, vector<Cards>& deck, User* user);
     //Set 2 cards each from Deck vector into:
     //Dealer's hand vector - One set to visible in UI
@@ -49,9 +63,32 @@ public:
     void addCard(vector<Cards>& currHand, vector<Cards>& deck);
     //Called when adding cards from Deck to hands
 
-    void getUserHandVal();
+    int getUserHandVal(User&);
 
-    void setUserHandVal();
+    //void setUserHandVal();
+    void compareCards(vector<Cards>& deck, User&);
+
+    void gameLoop(vector<Cards>& deck, User&);
+
+    void hit(vector<Cards>& deck);
+
+    void stand();
+
+
+    void bust(User& user);
+
+    int getHandValue();
+    bool hitIfNeeded(std::vector<Cards>& deck);
+
+    vector<Cards>& getDealerHand();
+
+    int getDealerHandVal() const;
+
+
+
+signals:
+    void endGame();
+
 };
 
 #endif //_DEALER_H
